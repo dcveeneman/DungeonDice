4fbbe4e8bde2a54efe60f6d6cc2fd35849a73136
David Veeneman <davidv@veeneman.com>
2023-11-22 12:02:51 -0800

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by David Veeneman on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
